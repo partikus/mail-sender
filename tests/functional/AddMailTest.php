5f9a79f21f504426c17860f4d27e7c8f84d5a465
Dominik Hajduk <dominikhajduk@gmail.com>
2015-09-19 17:35:00 +0200

    Add test for add single email function

--- tests/functional/AddMailTest.php
+++ tests/functional/AddMailTest.php
@@ -20,16 +20,30 @@ class AddMailTest extends PHPUnit_Extensions_Selenium2TestCase
     }
 
     /**
-     * @test
+     * @dataProvider emailProvider
      */
-    public function addMail()
+    public function testAddMail($mail, $expectedMessage)
     {
         $this->url('/mail_sender.php?page=add_mail');
+
+        $emailField = $this->byName('n_mail');
+        $emailField->value($mail);
 
         $button = $this->byName('add_mail');
 
         $button->click();
 
-        $this->assertContains('Niepoprawne mail!', $this->source());
+        $this->assertContains($expectedMessage, $this->source());
+    }
+
+    public function emailProvider()
+    {
+        return array(
+            array('', 'Niepoprawne mail!'),
+            array('some@', 'Niepoprawne mail!'),
+            array('some@email', 'Niepoprawne mail!'),
+            array('some@email.com', 'Maila dodano do bazy!'),
+            array('some@email.com', 'Zapytanie ofertowe wysłano ponownie!'),
+        );
     }
 }
